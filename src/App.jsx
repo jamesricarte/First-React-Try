0d6db8a96a838c58800d60405454f09241ca3917
Artistryyyy <07206442@dwc-legazpi.edu>
2024-09-28 22:13:20 +0800

    first change

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='header flex justify-between items-center bg-blue-400'>
+            <p>Divine</p>
+            <ul className='flex gap-2'>
+              <li>try</li>
+              <li>try</li>
+              <li>try</li>
+              <li>try</li>
+              <li>try</li>
+            </ul>
+          <button>james</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
