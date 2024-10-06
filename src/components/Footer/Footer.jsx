f28f8037783aebd67df5e1b72030e5c01ae14407
James Ricarte <uhenyou@gmail.com>
2024-10-07 00:08:25 +0800

    footer final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="h-[198px] bg-primary-100 flex justify-between items-center">
-        <div className="font-Poppins ml-[18vw]">
+        <div className="font-Poppins ml-[26vh]">
           <h1 className="font-bold text-black text-[40px] mb-0.5">
             Sign Up For Newsletter
           </h1>
@@ -12,7 +12,7 @@ const Footer = () => {
             Get E-mail updates about our latest shop and special orders
           </h3>
         </div>
-        <div className="mr-[18vw]">
+        <div className="mr-[26vh]">
           <form action="">
             <input
               className="p-3 rounded-l-lg w-[374px] font-bold placeholder-gray-300"
